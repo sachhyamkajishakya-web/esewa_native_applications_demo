8cdca3726d5802f43fa0c9fc80eeb7795c3b5565
Sachhyam Kaji Shakya <sachhyamkaji.shakya@gmail.com>
2026-03-03 17:03:08 +0545

    fix: add esewa_ios_app as regular folder

---
+++ esewa_ios_app/esewa_ios_appTests/esewa_ios_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  esewa_ios_appTests.swift
+//  esewa_ios_appTests
+//
+//  Created by Sachhyam Kaji Shakya on 03/03/2026.
+//
+
+import Testing
+
+struct esewa_ios_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
